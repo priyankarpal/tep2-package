21b798184b9065652bfae39b30f63b706c098fa1
priyankarpal <88102392+priyankarpal@users.noreply.github.com>
2024-06-18 17:10:18 +0530

    added current dir

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 #!/usr/bin/env node
 
 const { exec }=require('child_process');
+const path=require('path');
 
 const repo='https://github.com/priyankarpal/tep2';
 
-exec(`git clone ${repo}`, (err, stdout, stderr) => {
+
+const currentDir=process.cwd();
+
+
+process.chdir(currentDir);
+
+console.log('Hello from etp2 🔥');
+
+exec(`git clone ${repo} .`, (err, stdout, stderr) => {
     if (err) {
         console.error('Failed to clone repo:', err);
         return;
     }
     console.log(stdout);
-    console.log('Repo cloned successfully!');
+    console.log('Repo cloned successfully! 🔥');
     console.error(stderr);
 });
